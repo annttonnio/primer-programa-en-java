e3d5b4240c59803908faabded204ab80d87b0b16
annttonnio <antonio56.lop@gmail.com>
2021-10-13 13:34:54 +0200

    +cantante

--- prueba.java
+++ prueba.java
@@ -3,9 +3,10 @@ public class prueba
 
 	public static void main(String args []) 
 	{
-		System.out.println("Antonio López");
+/*		System.out.println("Antonio López"); */
 		System.out.println("León, 2002");
-		System.out.println("León, Windows 10 pro");
+		System.out.println("León, Windows 10");
+		System.out.println("Hendrix");
 	}
 
 }
